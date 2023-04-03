05d261387df7c83b284b5bf1d136f6712cf47fb2
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-03 17:41:25 +0300

    phase 15

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -1,25 +1,27 @@
 pipeline {
     agent any
     stages {
+        stage('Git credentials') {
+            steps {
+                git credentialsId: '04fe517d-3cf7-4aa3-8b40-5aa169e7e973',
+                    url: 'git@github.com:bombey77/twobit.git',
+                    branch: 'main'
+            }
+        }
         stage('Clean GIT branches') {
             steps {
                 script {
                     def repository_names = ["twobit"]
                     def src = "src"
-
                     if (!fileExists(src)) {
                         sh "mkdir $src"
                     } else {
                         deleteDir()
                         sh "mkdir $src"
                     }
-
                     dir(src) {
                         for (def repository : repository_names) {
                             def git_repository = "git@github.com:bombey77/${repository}.git"
-                            git credentialsId: '04fe517d-3cf7-4aa3-8b40-5aa169e7e973',
-                                url: 'git@github.com:bombey77/twobit.git',
-                                branch: 'main'
                             sh "git clone ${git_repository}"
                             println "Cloned from ${git_repository}"
                             dir(repository) {
