2406e428b3e1cb7ed349784b4b92f208707219d2
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-03 17:50:14 +0300

    phase 16

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -1,6 +1,11 @@
 pipeline {
     agent any
     stages {
+        stage('Clean workspace') {
+            steps {
+                cleanWs()
+            }
+        }
         stage('Git credentials') {
             steps {
                 git credentialsId: '04fe517d-3cf7-4aa3-8b40-5aa169e7e973',
