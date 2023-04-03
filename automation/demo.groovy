4e4250e44534f50ba39ed11fc705fe9f1785ac5f
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-04 00:59:35 +0300

    phase 21

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -18,6 +18,14 @@ pipeline {
                             git credentialsId: '04fe517d-3cf7-4aa3-8b40-5aa169e7e973',
                                 url: 'git@github.com:bombey77/twobit.git'
                             println "Cloned from ${git_repository}"
+                            dir(repository) {
+                                for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
+                                    if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) != 0) {
+                                        def remote_branch = branch.replaceAll("origin/", "")
+                                        println "Branch name to remove - ${remote_branch}"
+                                    }
+                                }
+                            }
                         }
                     }
                 }
