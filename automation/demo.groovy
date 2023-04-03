4353b774f9db31c8c0f92e18f7ca597b32708fdd
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-04 00:04:48 +0300

    phase 18

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -6,14 +6,32 @@ pipeline {
                 cleanWs()
             }
         }
-        stage('Git credentials') {
+        stage('Clean GIT branches') {
             steps {
-                git credentialsId: '04fe517d-3cf7-4aa3-8b40-5aa169e7e973',
-                    url: 'git@github.com:bombey77/twobit.git',
-                    branch: 'main'
+                script {
+                    def repository_names = ["twobit"]
+                    def src = "src"
+                    sh "mkdir $src"
+                    dir(src) {
+                        for (def repository : repository_names) {
+                            def git_repository = "git@github.com:bombey77/${repository}.git"
+                            git credentialsId: '04fe517d-3cf7-4aa3-8b40-5aa169e7e973',
+                                url: 'git@github.com:bombey77/twobit.git',
+                                branch: 'main'
+                            println "Cloned from ${git_repository}"
+                            dir(repository) {
+                                for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
+                                    if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) != 0) {
+                                        def remote_branch = branch.replaceAll("origin/", "")
+                                        println "Branch name to remove - ${remote_branch}"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-
     }
     post {
         always {
