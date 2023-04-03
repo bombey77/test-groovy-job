627c99cfe887a935be5f75707ced184be8872e77
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-03 13:14:41 +0300

    phase 9

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -1,31 +1,30 @@
-@NonCPS
-import org.jenkinsci.plugins.workflow.steps.CoreStep
-
 pipeline {
     agent any
     stages {
         stage('Clean GIT branches') {
             steps {
-                def repository_names = ["twobit", "sweater"]
-                def src = "src"
+                script {
+                    def repository_names = ["twobit", "sweater"]
+                    def src = "src"
 
-                if (!fileExists(src)) {
-                    sh "mkdir ${src}"
-                } else {
-                    deleteDir()
-                    sh "mkdir ${src}"
-                }
+                    if (!fileExists(src)) {
+                        mkdir src
+                    } else {
+                        deleteDir()
+                        mkdir src
+                    }
 
-                dir(src) {
-                    for (def repository : repository_names) {
-                        def git_repository = "git@github.com:bombey77/${repository}.git"
-                        sh "git clone ${git_repository}"
-                        println "Cloned from ${git_repository}"
-                        dir(repository) {
-                            for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
-                                if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) != 0) {
-                                    def remote_branch = branch.replaceAll("origin/", "")
-                                    println "Branch name to remove - ${remote_branch}"
+                    dir(src) {
+                        for (def repository : repository_names) {
+                            def git_repository = "git@github.com:bombey77/${repository}.git"
+                            sh "git clone ${git_repository}"
+                            println "Cloned from ${git_repository}"
+                            dir(repository) {
+                                for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
+                                    if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) != 0) {
+                                        def remote_branch = branch.replaceAll("origin/", "")
+                                        println "Branch name to remove - ${remote_branch}"
+                                    }
                                 }
                             }
                         }
