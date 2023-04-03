92e6d16a5120b9b450e4bc9347a728c909cdd19f
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-04 02:54:24 +0300

    phase 25

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -16,11 +16,10 @@ pipeline {
                         for (def repository : repository_names) {
                             def git_repository = "git@github.com:bombey77/${repository}.git"
                             git credentialsId: '04fe517d-3cf7-4aa3-8b40-5aa169e7e973',
-                                url: 'git@github.com:bombey77/twobit.git'
+                                url: '${git_repository}'
                             println "Cloned from ${git_repository}"
                             dir(repository) {
                                 for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
-                                    println "Br name - ${branch}"
                                     if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) == 0) {
                                         def remote_branch = branch.replaceAll("origin/", "")
                                         println "Branch name to remove - ${remote_branch}"
