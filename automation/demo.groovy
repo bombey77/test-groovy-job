613a9559707323bcaf63f1f2ae9b5d93803b9b6f
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-04 11:58:25 +0300

    phase 35

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -12,10 +12,12 @@ pipeline {
                     def repository_names = ["twobit"] 
                     dir("src") {
                         for (def repository : repository_names) {
-                            def git_repository = "git@github.com:bombey77/${repository}.git"
-                            git credentialsId: '04fe517d-3cf7-4aa3-8b40-5aa169e7e973',
-                                url: git_repository
-                            println "Cloned from ${git_repository}"
+                            dir(repository) {
+                                def git_repository = "git@github.com:bombey77/${repository}.git"
+                                git credentialsId: '04fe517d-3cf7-4aa3-8b40-5aa169e7e973',
+                                    url: git_repository
+                                println "Cloned from ${git_repository}"
+                            }
                         }
                     }
                 }
