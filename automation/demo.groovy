2b332d488d51922d435e9d61e5b1e70daae22c1e
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-05 02:51:29 +0300

    phase 119

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -16,18 +16,20 @@ pipeline {
                                     url: git_repository
                                 println "Cloned from ${git_repository}"
 
-                                def branches = sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split("\n")
-                                if (branches) {
-                                    branches.each { branch ->
-                                        def lastCommitDate = sh(script: "git log -1 --since='1 month ago' -s ${branch}", returnStdout: true).trim()
-                                        if (lastCommitDate.isEmpty()) {
-                                            def remoteBranch = branch.replaceAll("origin/", "")
-                                            println "Branch name to remove - ${remoteBranch}"
-                                            // sh(script: "git push origin -d ${remoteBranch}")
+                                def branchesStatus = sh(script: "git branch -r | grep -vE 'master|main'", returnStatus: true)
+                                if (branchesStatus == 0) {
+                                    def branches = sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split("\n")
+                                    for (branch in branches) {
+                                        def lastCommitDateStatus = sh(script: "git log -1 --since='1 month ago' -s ${branch}", returnStatus: true)
+                                        if (lastCommitDateStatus == 0) {
+                                            def lastCommitDate = sh(script: "git log -1 --since='1 month ago' -s ${branch}", returnStdout: true).trim()
+                                            if (lastCommitDate.isEmpty()) {
+                                                def remoteBranch = branch.replaceAll("origin/", "")
+                                                println "Branch name to remove - ${remoteBranch}"
+                                                // sh(script: "git push origin -d ${remoteBranch}")
+                                            }
                                         }
                                     }
-                                } else {
-                                    println "No branches found."
                                 }
                             }
                         }
