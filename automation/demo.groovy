b5f71fe7b4953a92751c84bb50b9fc0a1b5104b0
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-04 16:57:44 +0300

    phase 74

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -17,6 +17,7 @@ pipeline {
                         for (def repository : repository_names) {
                             def git_repository = "git@github.com:bombey77/${repository}.git"
                             sh "git clone ${git_repository}"
+                            println "Cloned from ${git_repository}"
                             dir(repository) {
                                 for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
                                     if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) == 0) {
