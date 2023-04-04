4e882c06e5108bde18304b87a30973d13b443c8a
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-05 00:56:03 +0300

    phase 103

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -17,6 +17,8 @@ pipeline {
                                 println "Cloned from ${git_repository}"
 
                                 for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
+                                    def test = sh(script: "git log -1 --since='1 month ago' -s ${branch}", returnStatus: true)
+                                    println "result = ${test}"
                                     if (sh(script: "git log -1 --since='1 month ago' -s ${branch}", returnStatus: true) != 0) {
                                         def remote_branch = branch.replaceAll("origin/", "")
                                         println "Branch name to remove - ${remote_branch}"
