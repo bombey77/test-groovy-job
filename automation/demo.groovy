4f257e031658907f1eb99097fca420790a243bc0
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-04 16:40:04 +0300

    phase 69

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -13,22 +13,24 @@ pipeline {
             steps {
                 script {
                     sshagent(['mac_ssh']) {
+                        def repository_names = ["twobit"]
+                        for (def repository : repository_names) {
+                            def git_repository = "git@github.com:bombey77/${repository}.git"
+                            sh "git clone ${git_repository}"
+                            dir(repository) {
+                                for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
+                                    if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) == 0) {
+                                        def remote_branch = branch.replaceAll("origin/", "")
+                                        println "Branch name to remove - ${remote_branch}"
+                                    }
+                                }
+                            }
+                        }
                         sh 'git clone git@github.com:bombey77/twobit.git'
                         sh 'pwd'
                         sh 'ls -la'
                         sh 'cd twobit'
                         sh 'ls -la'
-                    }
-                    def repository_names = ["twobit"]
-                    for (def repository : repository_names) {
-                        dir(repository) {
-                            for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
-                                if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) == 0) {
-                                    def remote_branch = branch.replaceAll("origin/", "")
-                                    println "Branch name to remove - ${remote_branch}"
-                                }
-                            }
-                        }
                     }
                 }
 //                script {
