a9130134d4fd2e0121833bb8b19d3a14241b3f1c
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-04 17:22:22 +0300

    phase 78

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -16,9 +16,16 @@ pipeline {
                             sh "git clone ${git_repository}"
                             println "Cloned from ${git_repository}"
                             dir(repository) {
-                                for (def branch : sh(script: "git branch --remote --list | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
-                                    if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) == 0) {
-                                        def remote_branch = branch.replaceAll("origin/", "")
+                                def branches = sh(script: "git branch --remote --list 'origin/*' | grep -vE 'master|main'", returnStdout: true).trim().split('\n')
+                                def latest_shas = sh(script: "git ls-remote --heads origin", returnStdout: true).trim().split('\n')
+                                        .collectEntries { line ->
+                                            def parts = line.split()
+                                            [parts[1].replaceAll('refs/heads/', ''): parts[0]]
+                                        }
+                                for (def branch : branches) {
+                                    def remote_branch = branch.replaceAll("origin/", "")
+                                    def latest_sha = latest_shas[remote_branch]
+                                    if (sh(script: "git log -1 --since='1 day ago' -s ${latest_sha}", returnStatus: true) == 0) {
                                         println "Branch name to remove - ${remote_branch}"
                                     }
                                 }
