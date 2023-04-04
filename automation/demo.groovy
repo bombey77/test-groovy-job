8ce62152061b4c8d3abe6759596f9662bb68701f
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-04 12:37:17 +0300

    phase 38

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -6,17 +6,16 @@ pipeline {
                 cleanWs()
             }
         }
-        stage('Clean GIT branches') {
+        stage('Clone GIT repositories and clean branches') {
             steps {
                 script {
                     def repository_names = ["twobit", "sweater"] 
-                    dir("src") {
-                        for (def repository : repository_names) {
+                    for (def repository : repository_names) {
+                        stage("Clone and clean ${repository}") {
                             dir(repository) {
                                 def git_repository = "git@github.com:bombey77/${repository}.git"
                                 git credentialsId: '04fe517d-3cf7-4aa3-8b40-5aa169e7e973',
-                                    url: git_repository,
-                                    refspec: '+refs/heads/*:refs/remotes/origin/*'
+                                    url: git_repository
                                 println "Cloned from ${git_repository}"
 
                                 for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
