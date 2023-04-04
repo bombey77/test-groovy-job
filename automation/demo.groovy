c2310ff8fede4530c51309ee4bc77627f2b6a949
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-04 11:55:57 +0300

    phase 34

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -9,28 +9,13 @@ pipeline {
         stage('Clean GIT branches') {
             steps {
                 script {
-                    def repository_names = ["twobit"]
-                    def src = "src"
-                    sh "mkdir $src"
-                    
-                    dir(src) {
+                    def repository_names = ["twobit"] 
+                    dir("src") {
                         for (def repository : repository_names) {
                             def git_repository = "git@github.com:bombey77/${repository}.git"
                             git credentialsId: '04fe517d-3cf7-4aa3-8b40-5aa169e7e973',
                                 url: git_repository
                             println "Cloned from ${git_repository}"
-                            
-                            sh "pwd"
-                            dir(repository) {
-                                sh "pwd"
-                                for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
-                                    if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) == 0) {
-                                        def remote_branch = branch.replaceAll("origin/", "")
-                                        println "Branch name to remove - ${remote_branch}"
-                                    }
-                                }
-                            }
-                            dir('..') {}
                         }
                     }
                 }
