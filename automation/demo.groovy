e79ff4cb30a8e36485f4966cd6adc4663f8f5459
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-04 18:10:16 +0300

    phase 82

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -9,16 +9,15 @@ pipeline {
                         dir(repository) {
                             def git_repository = "git@github.com:bombey77/${repository}.git"
                             git credentialsId: 'mac_ssh',
-                                    url: git_repository,
-                                    extensions: [[$class: 'CloneOption', noTags: true]]
+                                url: git_repository
                             println "Cloned from ${git_repository}"
 
-                            for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
-                                if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) == 0) {
-                                    def remote_branch = branch.replaceAll("origin/", "")
-                                    println "Branch name to remove - ${remote_branch}"
-                                }
-                            }
+//                            for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
+//                                if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) == 0) {
+//                                    def remote_branch = branch.replaceAll("origin/", "")
+//                                    println "Branch name to remove - ${remote_branch}"
+//                                }
+//                            }
                         }
                         stage("Clean workspace after ${repository}") {
                             cleanWs()
