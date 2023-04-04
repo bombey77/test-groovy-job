2e80efc70e01017ae9b5216cd80606c673cc6e06
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-04 18:19:06 +0300

    phase 85

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -7,21 +7,21 @@ pipeline {
                     def repository_names = ["twobit", "sweater"]
                     for (def repository : repository_names) {
                         dir(repository) {
-//                            def git_repository = "git@github.com:bombey77/${repository}.git"
-//                            git credentialsId: 'mac_ssh',
-//                                url: git_repository
-//                            println "Cloned from ${git_repository}"
+                            def git_repository = "git@github.com:bombey77/${repository}.git"
+                            git credentialsId: 'mac_ssh',
+                                url: git_repository
+                            println "Cloned from ${git_repository}"
 
-//                            for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
-//                                if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) == 0) {
-//                                    def remote_branch = branch.replaceAll("origin/", "")
-//                                    println "Branch name to remove - ${remote_branch}"
-//                                }
-//                            }
+                            for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
+                                if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) == 0) {
+                                    def remote_branch = branch.replaceAll("origin/", "")
+                                    println "Branch name to remove - ${remote_branch}"
+                                }
+                            }
                         }
-//                        stage("Clean workspace after ${repository}") {
-//                            cleanWs()
-//                        }
+                        stage("Clean workspace after ${repository}") {
+                            cleanWs()
+                        }
                     }
                 }
             }
