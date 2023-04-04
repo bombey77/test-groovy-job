ebcc27a746c3ff39c17512ffa6cc201883002b14
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-05 02:54:46 +0300

    phase 120

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -18,11 +18,11 @@ pipeline {
 
                                 def branchesStatus = sh(script: "git branch -r | grep -vE 'master|main'", returnStatus: true)
                                 if (branchesStatus == 0) {
-                                    def branches = sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split("\n")
+                                    def branches = branchesStatus.trim().split("\n")
                                     for (branch in branches) {
                                         def lastCommitDateStatus = sh(script: "git log -1 --since='1 month ago' -s ${branch}", returnStatus: true)
                                         if (lastCommitDateStatus == 0) {
-                                            def lastCommitDate = sh(script: "git log -1 --since='1 month ago' -s ${branch}", returnStdout: true).trim()
+                                            def lastCommitDate = lastCommitDateStatus.trim()
                                             if (lastCommitDate.isEmpty()) {
                                                 def remoteBranch = branch.replaceAll("origin/", "")
                                                 println "Branch name to remove - ${remoteBranch}"
