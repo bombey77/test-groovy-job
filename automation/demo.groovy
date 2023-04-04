78f07175731f0c233caa7b02173310e04d684980
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-04 13:03:14 +0300

    phase 43

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -1,34 +1,34 @@
 pipeline {
     agent any
-    options {
-        skipDefaultCheckout()
-    }
     stages {
         stage('Clean workspace') {
             steps {
                 cleanWs()
             }
         }
-        def repository_names = ["twobit", "sweater"] 
-        for (def repository : repository_names) {
-            stage("Clone and clean ${repository}") {
-                steps {
-                    script {
-                        def git_repository = "git@github.com:bombey77/${repository}.git"
-                        git credentialsId: '04fe517d-3cf7-4aa3-8b40-5aa169e7e973',
-                            url: git_repository,
-                            branch: '*/main'
-                        println "Cloned from ${git_repository}"
+        stage('Clone GIT repositories and clean branches') {
+            steps {
+                script {
+                    def repository_names = ["twobit", "sweater"] 
+                    for (def repository : repository_names) {
+                        stage("Clone and clean ${repository}") {
+                            dir(repository) {
+                                def git_repository = "git@github.com:bombey77/${repository}.git"
+                                git credentialsId: '04fe517d-3cf7-4aa3-8b40-5aa169e7e973',
+                                    url: git_repository
+                                println "Cloned from ${git_repository}"
 
-                        for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
-                            if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) == 0) {
-                                def remote_branch = branch.replaceAll("origin/", "")
-                                println "Branch name to remove - ${remote_branch}"
+                                for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
+                                    if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) == 0) {
+                                        def remote_branch = branch.replaceAll("origin/", "")
+                                        println "Branch name to remove - ${remote_branch}"
+                                    }
+                                }
                             }
                         }
                     }
                 }
-            }           
+            }
         }
     }
     post {
