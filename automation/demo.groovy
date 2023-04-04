32a75d6f235dccd86983662f5e2c4d87f5b885b8
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-04 16:30:53 +0300

    phase 68

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -11,12 +11,25 @@ pipeline {
         }
         stage('Clone GIT repositories and clean branches') {
             steps {
-                sshagent(['mac_ssh']) {
-                    sh 'git clone git@github.com:bombey77/sweater.git'
-                    sh 'pwd'
-                    sh 'ls -la'
-                    sh 'cd sweater'
-                    sh 'ls -la'
+                script {
+                    sshagent(['mac_ssh']) {
+                        sh 'git clone git@github.com:bombey77/twobit.git'
+                        sh 'pwd'
+                        sh 'ls -la'
+                        sh 'cd twobit'
+                        sh 'ls -la'
+                    }
+                    def repository_names = ["twobit"]
+                    for (def repository : repository_names) {
+                        dir(repository) {
+                            for (def branch : sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split('\n')) {
+                                if (sh(script: "git log -1 --since='1 day ago' -s ${branch}", returnStatus: true) == 0) {
+                                    def remote_branch = branch.replaceAll("origin/", "")
+                                    println "Branch name to remove - ${remote_branch}"
+                                }
+                            }
+                        }
+                    }
                 }
 //                script {
 //                    def repository_names = ["twobit", "sweater"]
