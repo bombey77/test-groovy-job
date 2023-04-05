9a783dbc2e81da4842500ee41e020767fc413a26
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-05 03:34:00 +0300

    phase 126

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -31,18 +31,18 @@ pipeline {
 //                                        }
 //                                    }
 //                                }
-                                def branches = sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split("\n")
-                                        .findAll { branch ->
-                                            def lastCommitDateStatus = sh(script: "git log -1 --since='1 month ago' -s ${branch}", returnStatus: true)
-                                            lastCommitDateStatus == 0 && sh(script: "git log -1 --since='1 month ago' -s ${branch}", returnStdout: true).trim().isEmpty()
-                                        }.collect { branch ->
-                                    branch.replaceAll("origin/", "")
-                                }
-
-                                if (!branches.isEmpty()) {
-                                    branches.each { branch ->
-                                        println "Branch name to remove - ${branch}"
-                                        // sh(script: "git push origin -d ${branch}")
+                                def branchesStatus = sh(script: "git branch -r | grep -vE 'master|main'", returnStatus: true)
+                                if (branchesStatus == 0) {
+                                    def branches = sh(script: "git branch -r | grep -vE 'master|main'", returnStdout: true).trim().split("\n")
+                                    def branchesToRemove = branches.findAll { branch ->
+                                        def lastCommitDateStatus = sh(script: "git log -1 --since='1 month ago' -s ${branch}", returnStatus: true)
+                                        lastCommitDateStatus == 0 && sh(script: "git log -1 --since='1 month ago' -s ${branch}", returnStdout: true).trim().isEmpty()
+                                    }.collect { branch -> branch.replaceAll("origin/", "") }
+                                    if (!branchesToRemove.isEmpty()) {
+                                        branchesToRemove.each { branch ->
+                                            println "Branch name to remove - ${branch}"
+                                            // sh(script: "git push origin -d ${branch}")
+                                        }
                                     }
                                 }
                             }
