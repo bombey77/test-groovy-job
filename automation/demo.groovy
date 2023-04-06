e7492c69d8df7c385a1cc9baf8bc94cb77a206e1
Els-Nix-CP-motovilovr <motovilovr@science.regn.net>
2023-04-06 18:45:16 +0300

    phase 155

--- automation/demo.groovy
+++ automation/demo.groovy
@@ -17,30 +17,30 @@ pipeline {
                                 def git_repository = "git@github.com:bombey77/${repository}.git"
                                 git credentialsId: 'mac_ssh',
                                     url: git_repository,
-                                    branch: 'main'
+                                    branch: getGitMainBranch(repository)
                                 println "Cloned from ${git_repository}"
 
-                               def remoteBranches = "git branch -r | grep -vE 'master|main'"
-                               def branchesStatus = sh(script: remoteBranches, returnStatus: true)
-                               if (branchesStatus == 0) {
-                                   def branches = sh(script: remoteBranches, returnStdout: true).trim().split("\n").findAll { it != null && it != '' }
+                                def remoteBranches = "git branch -r | grep -vE 'master|main'"
+                                def branchesStatus = sh(script: remoteBranches, returnStatus: true)
+                                if (branchesStatus == 0) {
+                                    def branches = sh(script: remoteBranches, returnStdout: true).trim().split("\n").findAll { it != null && it != '' }
 
-                                   def recentBranchLog = { branch ->
-                                       "git log -1 --since='1 month ago' -s ${branch}"
-                                   }
+                                    def recentBranchLog = { branch ->
+                                        "git log -1 --since='1 month ago' -s ${branch}"
+                                    }
 
-                                   branches.findAll { branch ->
-                                       def lastCommitDateStatus = sh(script: recentBranchLog(branch), returnStatus: true)
-                                       lastCommitDateStatus == 0
-                                   }.each { branch ->
-                                       def lastCommitDate = sh(script: recentBranchLog(branch), returnStdout: true).trim()
-                                       if (lastCommitDate.isEmpty()) {
-                                           def remoteBranch = branch.replaceAll("origin/", "")
-                                           println "Branch name to remove - ${remoteBranch}"
+                                    branches.findAll { branch ->
+                                        def lastCommitDateStatus = sh(script: recentBranchLog(branch), returnStatus: true)
+                                        lastCommitDateStatus == 0
+                                    }.each { branch ->
+                                        def lastCommitDate = sh(script: recentBranchLog(branch), returnStdout: true).trim()
+                                        if (lastCommitDate.isEmpty()) {
+                                            def remoteBranch = branch.replaceAll("origin/", "")
+                                            println "Branch name to remove - ${remoteBranch}"
                                            // sh(script: "git push origin -d ${remoteBranch}")
-                                       }
-                                   }
-                               }
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
